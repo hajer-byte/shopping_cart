c67200b953c2046d0753bf5a0f831ec530b652e2
hajer.mejri@contentpepper.de <hajer.mejri@contentpepper.de>
2021-01-15 21:57:22 +0100

    bugfixig

--- js/cart.js
+++ js/cart.js
@@ -1,53 +1,97 @@
-
-const addToCart = document.querySelectorAll(".btnPlus");
+//const addToCart = document.querySelectorAll(".btnPlus");
 //console.log(addToCart);
 //console.log(Array.from(addToCart));
-Array.from(addToCart).map((el) => {
-el.addEventListener("click",() => {
-    el.innerHTML="added ✌️";
+/*Array.from(addToCart).map((el) => {
+  el.addEventListener("click", () => {
+    el.innerHTML = "added ✌️";
 
-    const divE = document.createElement('div');
+    const divE = document.createElement("div");
     //each element's name
-    const ElementName = document.querySelectorAll(".elementName").innerHTML;
-    //each price 
-    const price = document.querySelectorAll('#price').innerHTML;
+    const ElementName = Array.from(
+      document.querySelectorAll(".elementName").innerHTML
+    );
+    //each price
+    const price = Array.from(document.querySelectorAll("#price").innerHTML);
     //total price
-    const total = Number(document.querySelectorAll('.total').textContent);
-    total.innerHTML=parseInt(total.innerHTML)+parseInt(el.nextElementSibling.innerHTML);
-    divE.className = 'ContentAdded alert alert-light parentELem p-0';
+    const total = Number(document.querySelectorAll(".total").textContent);
+    total.innerHTML =
+      parseInt(total.innerHTML) + parseInt(el.nextElementSibling.innerHTML);
+    divE.className = "ContentAdded alert alert-light parentELem p-0";
 
-  divE.innerHTML = `
- ${ElementName} that costs: ${price}
-  <input class="quantity" id="id_form-0-quantity" min="0" 
-  name="form-0-quantity" value="1" type="number" 
-  style="width:4.3em; height:2.3rem;">
+    divE.innerHTML = `<div class="alert alert-info" role="alert"> a new ${ElementName} is added '</div>
   `;
-  document.querySelector('#CART').appendChild(divE);
-});
-})
+    document.querySelector("#CART").appendChild(divE);
+  });
+});*/
 /////////////////////////////////////////////////////////////////
-const removeButn = document.querySelectorAll('.btnMn');
+
+/*const removeButn = document.querySelectorAll(".btnMn");
 
 Array.from(removeButn).map((el) => {
-el.addEventListener("click",() => {
-    el.innerHTML="removed ✌️";
-    document.querySelector('.ContentAdded').remove();
-  })
-})
+  el.addEventListener("click", () => {
+    el.innerHTML = "removed ✌️";
+    document.querySelector(".ContentAdded").remove();
+  });
+});*/
 /////////////////////////////////////////////////////////////////
-const removeButton = document.querySelectorAll('.removeBUTTON');
+
+const removeButton = document.querySelectorAll(".removeBUTTON");
 Array.from(removeButton).map((el) => {
   el.addEventListener("click", () => {
     el.parentElement.remove();
-  })
-})
+  });
+});
 
 ////////////////////////////////////////////////////////////////
-const heartBtn = document.querySelectorAll("#imgH");
-//console.log(heartBtn)
-Array.from(heartBtn).map((el) => {
-  el.addEventListener("click", () => {
-    el.style.filter = "invert(90%) Sepia(90%) saturate(3207%) hue-rotate(240deg) brightness(90%) contrast(90%)";
+let hearts = document.getElementsByClassName("fas fa-heart");
+
+for (let x of hearts) {
+  x.addEventListener("click", () => {
+    if (x.style.color == "red") {
+      x.style.color = "gray";
+    } else {
+      x.style.color = "red";
+    }
   });
-})
+}
 /////////////////////////////////////////////////////////////////
+
+let buttonremove = document.getElementsByClassName("fas fa-trash");
+
+for (let x of buttonremove) {
+  x.addEventListener("click", function () {
+    x.parentNode.parentElement.remove();
+    somme();
+  });
+}
+///////////////////////////////////////////////////////////////////
+
+let buttonplus = document.getElementsByClassName("add");
+for (let x of buttonplus) {
+  x.addEventListener("click", function () {
+    x.nextElementSibling.innerHTML++;
+    somme();
+  });
+}
+///////////////////////////////////////////////////////////////////
+
+let buttonmoins = document.getElementsByClassName("moins");
+for (let x of buttonmoins) {
+  x.addEventListener("click", function () {
+    if (x.previousElementSibling.innerHTML > 0) {
+      x.previousElementSibling.innerHTML--;
+      somme();
+    }
+  });
+}
+///////////////////////////////////////////////////////////////////
+somme = function () {
+  let achat = Array.from(document.getElementsByClassName("achat"));
+  let prix = Array.from(document.getElementsByClassName("prix"));
+  let sum = 0;
+
+  for (let i = 0; i < achat.length; i++) {
+    sum += achat[i].innerHTML * prix[i].innerHTML;
+  }
+  document.getElementById("total").innerHTML = sum;
+};
